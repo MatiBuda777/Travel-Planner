a5b3638851130ffe8adc17dfff2b108149f7a557
Mateusz <mateusz.budek@cyfroweszkoly.pl>
2025-04-29 16:05:25 +0200

    First Commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Avalonia.Controls;
+
+namespace Travel_Planner;
+
+public partial class MainWindow : Window
+{
+    private List<string> _countries = new List<string>
+    {
+        "Afganistan", "Albania", "Algieria", "Andora", "Angola", "Antigua i Barbuda", "Arabia Saudyjska", "Argentyna",
+        "Armenia", "Australia", "Austria", "Azerbejdżan", "Bahamy", "Bahrajn", "Bangladesz", "Barbados", "Belgia",
+        "Belize", "Benin", "Bhutan", "Białoruś", "Boliwia", "Bośnia i Hercegowina", "Botswana", "Brazylia", "Brunei",
+        "Bułgaria", "Burkina Faso", "Burundi", "Chile", "Chiny", "Cypr", "Czarnogóra", "Czechy", "Dania",
+        "Demokratyczna Republika Konga", "Dominika", "Dominikana", "Dżibuti", "Egipt", "Ekwador", "Erytrea", "Estonia",
+        "Eswatini", "Etiopia", "Fidżi", "Filipiny", "Finlandia", "Francja", "Gabon", "Gambia", "Ghana", "Grecja",
+        "Grenada", "Gruzja", "Guatemala", "Gwinea", "Gwinea Bissau", "Gwinea Równikowa", "Haiti", "Hiszpania",
+        "Holandia", "Honduras", "Indie", "Indonezja", "Irak", "Iran", "Irlandia", "Islandia", "Izrael", "Jamajka",
+        "Japonia", "Jemen", "Jordania", "Kambodża", "Kamerun", "Kanada", "Katar", "Kazachstan", "Kenia", "Kirgistan",
+        "Kiribati", "Kolumbia", "Komory", "Kongo", "Korea Południowa", "Korea Północna", "Kosowo", "Kostaryka", "Kuba",
+        "Kuwejt", "Laos", "Lesotho", "Liban", "Liberia", "Libia", "Liechtenstein", "Litwa", "Luksemburg", "Łotwa",
+        "Madagaskar", "Malawi", "Malediwy", "Malezja", "Mali", "Malta", "Maroko", "Mauritius", "Mauretania", "Meksyk",
+        "Mołdawia", "Monako", "Mongolia", "Mozambik", "Mjanma", "Namibia", "Nauru", "Nepal", "Niemcy", "Niger",
+        "Nigeria", "Norwegia", "Nowa Zelandia", "Oman", "Pakistan", "Panama", "Papua-Nowa Gwinea", "Paragwaj", "Peru",
+        "Polska", "Portugalia", "Republika Południowej Afryki", "Republika Środkowoafrykańska", "Rosja", "Rumunia",
+        "Rwanda", "Saint Kitts i Nevis", "Saint Lucia", "Saint Vincent i Grenadyny", "Salwador", "Samoa", "San Marino",
+        "Senegal", "Serbia", "Seszele", "Sierra Leone", "Singapur", "Słowacja", "Słowenia", "Somalia", "Sri Lanka",
+        "Sudan", "Sudan Południowy", "Surinam", "Syria", "Tadżykistan", "Tajlandia", "Tanzania", "Timor Wschodni",
+        "Togo", "Tonga", "Trynidad i Tobago", "Tunezja", "Turcja", "Turkmenistan", "Tuvalu", "Uganda", "Ukraina",
+        "Urugwaj", "USA", "Uzbekistan", "Vanuatu", "Watykan", "Wenezuela", "Węgry", "Wielka Brytania", "Wietnam",
+        "Wybrzeże Kości Słoniowej", "Wyspy Marshalla", "Wyspy Salomona", "Zambia", "Zimbabwe"
+    };
+    
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        foreach (var country in _countries)
+        {
+            var newItem = new ComboBoxItem();
+        }
+    }
+}
